db14d64823c1b1eb9bdcaf273cc1f9e19dca22c0
nermin <nerminhassan2820@gmail.com>
2023-12-12 19:24:12 +0200

    Create Razor project & Db

---
+++ BulkyWebRazor/Migrations/20231212172321_addCategoryToDb.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWebRazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategoryToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    displayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name", "displayOrder" },
+                values: new object[,]
+                {
+                    { 1, "Cat1", 1 },
+                    { 2, "Cat2", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ BulkyWebRazor/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [Range(1, 100, ErrorMessage ="Display order must be between 1-100")]
+        [DisplayName("Display Order")]
+        public int displayOrder { get; set; }
+    }
+}
