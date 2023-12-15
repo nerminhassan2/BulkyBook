a7ddbdd074101ad9cb306f3471a82987e0bcbdc6
nermin <nerminhassan2820@gmail.com>
2023-12-15 19:18:17 +0200

    Product Index

---
+++ Bulky/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index() 
+        { 
+            List<Product> products = _unitOfWork.ProductRepository.GetAll().ToList();
+            return View(products);
+        }
+
+
+
+
+    }
+}
