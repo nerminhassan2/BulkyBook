69c5f5cd3ad1b000dc2409316ac99fc38fcca25e
nermin <nerminhassan2820@gmail.com>
2023-12-15 16:10:18 +0200

    Create Product Model

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]  
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        [Display(Name ="Price for 1-50")]
+        [Range(1,1000)]
+        public int Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 50+")]
+        [Range(1, 1000)]
+        public int Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price for 100+")]
+        [Range(1, 1000)]
+        public int Price100 { get; set; }
+
+    }
+}
