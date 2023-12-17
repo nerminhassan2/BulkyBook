7bec6f4fa2ff32b1d7513f39b4d3aca03957f45d
nermin <nerminhassan2820@gmail.com>
2023-12-17 02:01:44 +0200

    Edit in DataTables

--- Bulky/wwwroot/js/Product.js
+++ Bulky/wwwroot/js/Product.js
@@ -10,7 +10,17 @@ function loadDataTable() {
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "12%" }
+            { data: 'category.name', "width": "12%" },
+            {
+                data: 'productId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/Admin/Product/Upsert?productId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "20%"
+            }
 
         ]
     });
